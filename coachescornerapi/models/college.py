6d94439d86e3d48b2f48f94dc7eaa6f7c40f2b6f
Stacy Lofton <lofton713@gmail.com>
2022-09-09 09:02:47 -0500

    all fixtures loaded

--- coachescornerapi/models/college.py
+++ coachescornerapi/models/college.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class College(models.Model):
     name = models.CharField(max_length=50)
-    location = models.CharField (max_length=50)
+    city = models.CharField (max_length=50)
+    state = models.CharField(max_length=50)
     min_GPA = models.FloatField()
     
--- coachescornerapi/models/game.py
+++ coachescornerapi/models/game.py
@@ -2,7 +2,8 @@ from django.db import models
 
 class Game(models.Model):
     player = models.ForeignKey("Player", on_delete=models.CASCADE)
-    location = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
     date = models.DateField()
     time = models.TimeField()
     description = models.TextField(max_length=100)
